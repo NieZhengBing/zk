d55f366d564985d39b11a80aec80ae24ba9f0f70
26278 <26278@DESKTOP-47DLIGG>
2017-12-24 15:42:26 +0800

    get data demo

---
+++ src/main/java/com/nzb/zk/client/GetDataDemo.java
@@ -0,0 +1,33 @@
+package com.nzb.zk.client;
+
+import org.I0Itec.zkclient.IZkDataListener;
+import org.I0Itec.zkclient.ZkClient;
+
+public class GetDataDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		String path = "/zk-client";
+		ZkClient client = new ZkClient("192.168.159.152:2181", 5000);
+		client.createEphemeral(path, "123");
+		
+		client.subscribeDataChanges(path, new IZkDataListener() {
+			
+			@Override
+			public void handleDataDeleted(String dataPath) throws Exception {
+				System.out.println(dataPath + " deleted");
+			}
+			
+			@Override
+			public void handleDataChange(String dataPath, Object data) throws Exception {
+				System.out.println(dataPath + " changed: " + data);
+			}
+		});
+		
+		System.out.println(client.readData(path).toString());
+		client.writeData(path, "456");
+		Thread.sleep(1000);
+		client.delete(path);
+		Thread.sleep(Integer.MAX_VALUE);
+	}
+
+}
